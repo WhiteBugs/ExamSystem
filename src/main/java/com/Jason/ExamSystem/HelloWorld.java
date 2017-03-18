137bd0f441725541fc1bb17bdd3f3b231172f94f
WhiteBugs <chengjianan1996@gmail.com>
2017-03-18 12:49:40 +0800

    配置成功
    可运行install命令后发布为war并复制到tomcat的webapps目录运行

---
+++ src/main/java/com/Jason/ExamSystem/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.Jason.ExamSystem;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Hello World</title></head>< body><p>hello world</p>< /body></html>");
+        
+	}
+
+}
