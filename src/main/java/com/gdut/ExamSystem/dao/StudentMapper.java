2f1ad697157eda9db3f377809f4aceeb26dfae1a
WhiteBugs <chengjianan1996@gmail.com>
2017-03-26 09:56:34 +0800

    配置mybatis-generator成功
    可利用mybatis-generator自动生成数据库代码

---
+++ src/main/java/com/gdut/ExamSystem/dao/StudentMapper.java
@@ -0,0 +1,9 @@
+package com.gdut.ExamSystem.dao;
+
+import com.gdut.ExamSystem.model.Student;
+
+public interface StudentMapper {
+    int insert(Student record);
+
+    int insertSelective(Student record);
+}
